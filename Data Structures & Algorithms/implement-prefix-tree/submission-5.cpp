3acce97d4127f1912766e2e80f84bec12c88f56a
minnie-chan <ploking64@gmail.com>
2026-03-30 20:30:37 -0700

    Add: implement-prefix-tree - submission-5

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-5.cpp
@@ -0,0 +1,50 @@
+class PrefixTree {
+public:
+
+    struct Node {
+        unordered_map<char, Node*> mp;
+        bool isEnd = false;
+    };
+    Node* root;
+    PrefixTree() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+
+        for(char c: word){
+            
+            if(node->mp.find(c) == node->mp.end()){
+                node->mp[c] = new Node();
+            }
+            node = node->mp[c];
+        }
+        node->isEnd = true;
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+
+        for (char c : word) {
+            if(node->mp.find(c) == node->mp.end()){
+                return false;
+            }
+            node = node->mp[c];
+        }
+        return node->isEnd;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node = root;
+
+        for (char c : prefix) {
+            if (node->mp.find(c) == node->mp.end()) {
+                return false;
+            }
+            node = node->mp[c];
+        }
+
+        return true;
+    }
+};

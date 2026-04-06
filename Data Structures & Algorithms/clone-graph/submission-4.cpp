d8f2e0af836de8972cba7df659e066d7777fe30a
minnie-chan <ploking64@gmail.com>
2026-04-05 22:54:47 -0700

    Add: clone-graph - submission-4

---
+++ Data Structures & Algorithms/clone-graph/submission-4.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(node == nullptr){
+            return nullptr;
+        }
+        unordered_map<Node*,Node*> mp;
+        return dfs(node,mp);
+    }
+    Node* dfs(Node* node, unordered_map<Node*,Node*>& mp){
+        if(mp.find(node) != mp.end()){
+            return mp[node];
+        }
+
+        Node* a = new Node(node->val);
+        mp[node] = a;
+
+        for(auto aa : node->neighbors){
+            a->neighbors.push_back(dfs(aa,mp));
+        }
+        return a;
+    }
+
+};

b44e2f05b6ab3c5a850a19fd61f3423c462648be
minnie-chan <ploking64@gmail.com>
2026-05-28 23:04:00 -0700

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> vec(m, vector<int>(n, -1));
+        return dfs(0,0,m,n,vec);
+    }
+    int dfs(int r, int c, int m, int n, vector<vector<int>>& vec){
+        if(r >= m || c >= n){
+            return 0;
+        }
+        if(vec[r][c] != -1){
+            return vec[r][c];
+        }
+       if(r == m -1 && c == n-1 ){
+            return 1;
+        }
+
+        int right = dfs(r + 1,c,m,n,vec);
+        int down = dfs(r,c + 1,m,n,vec);
+        vec[r][c] = right + down;
+        return vec[r][c];
+    }
+};

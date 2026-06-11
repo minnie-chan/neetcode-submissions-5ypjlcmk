52128a27c9423ec41c3b8e09599c4b1a4c426dbf
minnie-chan <ploking64@gmail.com>
2026-06-11 11:19:27 -0700

    Add: sum-of-two-integers - submission-2

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b != 0){
+            int bit = (a & b) << 1;
+            a = a ^ b;
+            b = bit;
+        }
+        
+        return a;
+    }
+};

227d864a35775780b3099cfa21d5f892f3187b8c
minnie-chan <ploking64@gmail.com>
2026-06-19 16:34:55 -0700

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+
+        for(auto& a : strs){
+            
+            string b = a;
+            sort(b.begin(), b.end());
+            mp[b].push_back(a);
+        } 
+
+        vector<vector<string>> vec;
+
+        for(auto& [key,value] : mp){
+            vec.push_back(value);
+        }
+        return vec;
+    }
+};

a9d947cd035d3fd1308c198378c9216fb5dd6066
minnie-chan <ploking64@gmail.com>
2026-06-24 17:44:25 -0700

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string ans = "";
+        for(auto& s : strs){
+
+            ans += to_string(s.size()) + "#" +  s;
+        }
+        return ans;
+        
+    }
+
+    vector<string> decode(string s) {
+        int i = 0;
+        vector<string> ans;
+        while(i < s.size()){
+            string a = "";
+            while(s[i] != '#'){
+                a += s[i];
+                i++;
+            }
+            i++;
+            int b = stoi(a);
+            string c = s.substr(i, b);
+            i += b;
+            ans.push_back(c);
+        }
+        return ans;
+    }
+};

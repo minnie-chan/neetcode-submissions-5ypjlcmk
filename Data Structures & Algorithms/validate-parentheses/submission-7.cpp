9d978378eee246914d7974a66a6fb085f69891bd
minnie-chan <ploking64@gmail.com>
2026-07-16 18:14:36 -0700

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(auto& a : s){
+            if(a == '(' || a == '{' || a == '['){
+                st.push(a);
+            } else {
+
+                if(st.empty()){
+                    return false;
+                } else if(st.top() == '(' && a == ')'){
+                    st.pop();
+                } else if(st.top() == '{' && a == '}'){
+                    st.pop();
+                } else if(st.top() == '[' && a == ']'){
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};

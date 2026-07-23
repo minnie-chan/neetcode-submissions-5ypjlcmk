8e9910888b50da513842cd9e0364a982738245bc
minnie-chan <ploking64@gmail.com>
2026-07-23 12:35:01 -0700

    Add: car-fleet - submission-7

---
+++ Data Structures & Algorithms/car-fleet/submission-7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,int>> vec;
+        int fleets = 1;
+        for(int i = 0 ; i < speed.size();i++){
+            vec.push_back({position[i],speed[i]});
+        }
+        sort(vec.begin(),vec.end());
+        int n = vec.size();
+        double time = (double)(target - vec[n-1].first) / vec[n-1].second;
+
+        for(int i = position.size() - 2; i >= 0; i--){
+            double nt = (double)(target - vec[i].first) / vec[i].second;
+
+            if(nt > time){
+                fleets++;
+                time = nt;
+            }
+        }
+        return fleets;
+        
+    }
+};

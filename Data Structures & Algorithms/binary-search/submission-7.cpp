dc0d659dc3558611c5bcf7c446dce0634fe5e08f
minnie-chan <ploking64@gmail.com>
2026-07-29 19:21:12 -0700

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while(l <= r){
+            int middle = l + (r - l) / 2 ;
+
+            if(nums[middle] == target){
+                return middle;
+            }
+
+            if(nums[middle] < target){
+                l = middle + 1;
+            } else {
+                r = middle - 1;
+            }
+        }
+        return -1;
+    }
+};

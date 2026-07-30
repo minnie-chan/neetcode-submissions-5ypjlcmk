3e74cc65382be4ec5c455740336e81c7093fc2b5
minnie-chan <ploking64@gmail.com>
2026-07-29 19:52:23 -0700

    Add: search-2d-matrix - submission-25

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-25.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int l = 0;
+        int r = matrix.size() - 1;
+
+        while(l <= r){
+            int middle = l + (r - l) / 2;
+
+            if(matrix[middle][0] > target){
+                r = middle - 1;
+            } else if(matrix[middle][0] < target){
+                l = middle + 1;
+            } else {
+                return true;
+            }
+        }
+
+        if(r == -1){
+            return false;
+        }
+        l = 0;
+        
+        int b = matrix[r].size() - 1;
+        while(l <= b){
+            int middle = l + (b - l) / 2;
+
+            if(matrix[r][middle]  > target){
+                b = middle - 1;
+            } else if(matrix[r][middle] < target){
+                l = middle + 1;
+            } else if(matrix[r][middle] == target){
+                return true;
+            }
+        } 
+        return false;
+    }
+};

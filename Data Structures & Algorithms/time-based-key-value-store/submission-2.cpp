c2e5915e98cc317f5684edf00a0a379426c29506
minnie-chan <ploking64@gmail.com>
2026-08-05 00:40:05 -0700

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cpp
@@ -0,0 +1,33 @@
+class TimeMap {
+public:
+    TimeMap() {
+        
+    }
+    unordered_map<string,vector<pair<int,string>>> mp;
+
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp,value});    
+    }
+    
+    string get(string key, int timestamp) {
+  if (mp.find(key) == mp.end()) {
+            return "";
+        }
+        string ans;
+        auto& vec = mp[key];
+        int l = 0;
+        int r = vec.size() - 1;
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+
+            if(vec[mid].first <= timestamp){
+                ans = vec[mid].second;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};

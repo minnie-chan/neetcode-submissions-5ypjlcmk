e77261356ec27a2a5e26a17c0a66245939950fe8
minnie-chan <ploking64@gmail.com>
2026-09-08 19:46:11 -0700

    Add: daily-temperatures - submission-12

---
+++ Data Structures & Algorithms/daily-temperatures/submission-12.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        vector<int> ans(temp.size(), 0);
+        stack<int> st;
+        st.push(0);
+        for (int i = 1; i < temp.size(); i++) {
+            
+            while(!st.empty() && temp[i] > temp[st.top()]){
+                ans[st.top()] = i - st.top();
+                st.pop();
+
+            } 
+            st.push(i);
+        }
+        return ans;
+    }
+};
